f1850bc241819b1352f283829225436fee2e7464
AdrianTodt <adriantodt.ms@gmail.com>
2020-08-27 10:46:03 -0300

    That's a better Mixin

--- src/main/java/net/adriantodt/fallflyinglib/mixin/LivingEntityMixin.java
+++ src/main/java/net/adriantodt/fallflyinglib/mixin/LivingEntityMixin.java
@@ -1,13 +1,20 @@
 package net.adriantodt.fallflyinglib.mixin;
 
 import net.adriantodt.fallflyinglib.impl.FallFlyingLibInternals;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
     @ModifyArg(
         at = @At(
             value = "INVOKE",
@@ -15,10 +22,9 @@ public abstract class LivingEntityMixin {
         ), method = "initAi", index = 1
     )
     private boolean injectAiFix(boolean value) {
-        EntityAccessor accessor = (EntityAccessor) this;
-        boolean bl = accessor.callGetFlag(7);
+        boolean bl = this.getFlag(7);
 
-        if (bl && !accessor.callIsOnGround() && !accessor.callHasVehicle()) {
+        if (bl && !this.isOnGround() && !this.hasVehicle()) {
             return FallFlyingLibInternals.isFallFlyingAllowed((LivingEntity) (Object) this) || value;
         }
         return value;
